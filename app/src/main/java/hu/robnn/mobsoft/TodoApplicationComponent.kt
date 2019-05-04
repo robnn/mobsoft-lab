afab8022d9486bef3952a80e7ad9e2f1142e01e6
Robin <robin.kurovszky@gmail.com>
2019-05-04 13:26:22 +0200

    Add api and orm layer

--- app/src/main/java/hu/robnn/mobsoft/TodoApplicationComponent.kt
+++ app/src/main/java/hu/robnn/mobsoft/TodoApplicationComponent.kt
@@ -1,5 +1,7 @@
 package hu.robnn.mobsoft
 
+import android.app.Application
+import dagger.BindsInstance
 import dagger.Component
 import hu.robnn.mobsoft.interactor.InteractorModule
 import hu.robnn.mobsoft.network.NetworkModule
@@ -11,6 +13,18 @@ import javax.inject.Singleton
 @Singleton
 @Component(modules = [UIModule::class, NetworkModule::class, InteractorModule::class])
 interface TodoApplicationComponent {
+
+    @Component.Builder
+    interface Builder {
+        @BindsInstance
+        fun application(application: Application): Builder
+
+        fun build(): TodoApplicationComponent
+
+        fun uIModule(uiModule: UIModule): Builder
+    }
+
     fun inject(todosFragment: TodosFragment)
     fun inject(createFragment: CreateFragment)
+    fun inject(application: TodoApplication)
 }
--- app/src/main/java/hu/robnn/mobsoft/model/Todo.kt
+++ app/src/main/java/hu/robnn/mobsoft/model/Todo.kt
@@ -1,11 +1,22 @@
 package hu.robnn.mobsoft.model
 
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+import android.support.annotation.NonNull
 import com.google.gson.annotations.SerializedName
 import java.util.*
 
+@Entity(tableName = "todo_table")
 data class Todo(
+
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "creation_date")
     @SerializedName("creationDate")
     var creationDate: Date? = null,
+
+    @NonNull
     @SerializedName("description")
     var description: String? = null
 )
